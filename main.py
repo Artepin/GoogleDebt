f1a877aab87cf264b9dc6f6ce3aeec9ecae9b9d6
Wolverine <artemkozlov68@gmail.com>
2021-07-16 14:38:06 +0300

    Добавлена первая версимя прохода по колонке со сменой цвета при совпадении условия

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import gspread
 import datetime
+import re
 gp = gspread.service_account(filename='./auth.json')
 spreadsheet = gp.open('TestParsing')
 worksheet = spreadsheet.get_worksheet(0)
@@ -17,6 +18,8 @@ if dateFact == None:
                 }
             }
                         )
+
+
 print(datePlan)
 def dateTransform(data):
     if data !='None':
@@ -35,12 +38,48 @@ def redOrYellow(data):
         print("Red color")
     else:
         print("Yellow color")
+
+def prov(data):
+    match =  re.search(r'\d{2}',data)
+    print ("true" if match else 'false')
+
+
+def changeOfColor(coord):
+    worksheet.format(coord, {
+                "backgroundColor": {
+                    "red": 1.0,
+                    "green": 0.0,
+                    "blue": 0.0
+                }
+            }
+                        )
+
+def prohod(dataColumn):
+    for i in dataColumn:
+        match = prov(dataColumn[i])
+        if match:
+            print("Match true")
+            cellCoord = 'E'+i
+            cell = worksheet.acell(cellCoord).value
+
+            if prov(cell):
+                print("change color")
+                changeOfColor(cellCoord)
+            else:
+                print("No date")
+        else:
+            print("Match False")
+
+
+column = worksheet.col_values(4)
+prohod(column)
+
+
 datePlanForm = dateTransform(datePlan)
 countDays = dateRazn(datePlanForm, dateNow)
 result = redOrYellow(countDays)
 print(datePlanForm)
 print(countDays)
 print(result)
-
-col = worksheet.col_values(4)
 print(col)
+print(prov(a1))
